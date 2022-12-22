a9f4968bcfba0e82789d7ed06f53f7c72d487bca
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-22 14:17:06 +0300

    Initial commit

---
+++ zadacha54/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.Clear();
+
+int Rand() {
+    Random rnd = new Random();
+    int num = rnd.Next(1, 10);
+    return num;
+}
+
+// void ShowArray(int[] arr) {
+//     for(int i = 0; i < 3; i++) {
+//         Console.Write("[");
+//         for(int o = 0; o < 4; o++) {
+//             if(o == 4 - 1) {
+//                 Console.WriteLine($"{arr[i, o]}]");
+//             } else {
+//                 Console.Write($"{arr[i, o]}, ");
+//             }
+//         }
+//     }
+// }
+
+int[,] CreateArray(int rows, int columns) {
+    // rows = int.Parse(Console.ReadLine());
+    // columns = int.Parse(Console.ReadLine());
+    int[,] arr = new int [rows, columns];
+
+    // var input = Console.ReadLine();
+    // var split = input.Split(" ");
+    // int rows = int.Parse(split[0]);
+    // int columns = int.Parse(split[1]);
+
+    for(int i = 0; i < rows; i++) {
+        // Console.Write("[");
+        for(int o = 0; o < columns; o++) {
+            arr[i, o] = Rand();
+            // if(o == columns - 1) {
+            //     Console.WriteLine($"{arr[i, o]}]");
+            // } else {
+            //     Console.Write($"{arr[i, o]}, ");
+            // }
+        }
+    }
+
+    return arr;
+}
+
+Console.Write(CreateArray(3, 4));
+
+// int[,] twoDarr = {{Rand(), Rand(), Rand(), Rand()},
+//                   {Rand(), Rand(), Rand(), Rand()},
+//                   {Rand(), Rand(), Rand(), Rand()}};
+
+// for(int i = 0; i < 3; i++) {
+//     for(int o = 0; o < 4; o++) {
+        
+//     }
+// }
