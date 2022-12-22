ba667736197e2f5f1d9b2955400e39bdc8c042f8
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-22 14:56:34 +0300

    wip zadacha54

--- zadacha54/Program.cs
+++ zadacha54/Program.cs
@@ -1,57 +1,33 @@
 ﻿Console.Clear();
 
-int Rand() {
-    Random rnd = new Random();
-    int num = rnd.Next(1, 10);
-    return num;
-}
-
-// void ShowArray(int[] arr) {
-//     for(int i = 0; i < 3; i++) {
-//         Console.Write("[");
-//         for(int o = 0; o < 4; o++) {
-//             if(o == 4 - 1) {
-//                 Console.WriteLine($"{arr[i, o]}]");
-//             } else {
-//                 Console.Write($"{arr[i, o]}, ");
-//             }
-//         }
-//     }
-// }
+Console.Write("Сначала введите количество строк и затем количество столбиков через пробел: ");
+var input = Console.ReadLine();
+var space = input.Split(" ");
+int rows = int.Parse(space[0]);
+int columns = int.Parse(space[1]);
 
 int[,] CreateArray(int rows, int columns) {
-    // rows = int.Parse(Console.ReadLine());
-    // columns = int.Parse(Console.ReadLine());
+    Random rnd = new Random();
     int[,] arr = new int [rows, columns];
-
-    // var input = Console.ReadLine();
-    // var split = input.Split(" ");
-    // int rows = int.Parse(split[0]);
-    // int columns = int.Parse(split[1]);
-
     for(int i = 0; i < rows; i++) {
-        // Console.Write("[");
         for(int o = 0; o < columns; o++) {
-            arr[i, o] = Rand();
-            // if(o == columns - 1) {
-            //     Console.WriteLine($"{arr[i, o]}]");
-            // } else {
-            //     Console.Write($"{arr[i, o]}, ");
-            // }
+            arr[i, o] = rnd.Next(1, 10);
         }
     }
-
     return arr;
 }
 
-Console.Write(CreateArray(3, 4));
+void ShowArray(int[,] arr) {
+    for(int i = 0; i < rows; i++) {
+        Console.Write("[");
+        for(int o = 0; o < columns; o++) {
+            if(o == columns - 1) {
+                Console.WriteLine($"{arr[i, o]}]");
+            } else {
+                Console.Write($"{arr[i, o]}, ");
+            }
+        }
+    }
+}
 
-// int[,] twoDarr = {{Rand(), Rand(), Rand(), Rand()},
-//                   {Rand(), Rand(), Rand(), Rand()},
-//                   {Rand(), Rand(), Rand(), Rand()}};
-
-// for(int i = 0; i < 3; i++) {
-//     for(int o = 0; o < 4; o++) {
-        
-//     }
-// }
+ShowArray(CreateArray(rows, columns));
