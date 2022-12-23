f864325874152f70b463f42d90d77412b4b9f45a
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-24 00:39:00 +0300

    cleaned zadacha60

--- zadacha60/Program.cs
+++ zadacha60/Program.cs
@@ -14,32 +14,6 @@ for(int i = 0; i < size; i++) {
     }
 }
 
-// // check for same numbers, not perfect
-// bool isSame(int num) {
-//     for(int q = 0; q < size; q++) {
-//         for(int w = 0; w < size; w++) {
-//             for(int e = 0; e < size; e++) {
-//                 if(arr3D[q, w, e] == num) {
-//                     return false;
-//                 }
-//             }
-//         }
-//     }
-//     return true;
-// }
-// for(int s = 0; s < 5; s++) {
-//     for(int i = 0; i < size; i++) {
-//         for(int j = 0; j < size; j++) {
-//             for(int u = 0; u < size; u++) {
-//                 if(isSame(arr3D[i, j,u]) == false) {
-//                     Random rnd = new Random();
-//                     arr3D[i, j, u] = rnd.Next(10, 100);
-//                 }
-//             }
-//         }
-//     }
-// }
-
 bool isSame(int num) {
     for(int q = 0; q < size; q++) {
         for(int w = 0; w < size; w++) {
